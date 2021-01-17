0b1ab723c6673395724dd38234fe994d6d4e837d
Sourav Goswami <souravgoswami@protonmail.com>
2021-01-18 02:52:17 +0530

    Updated test.rb

--- test/test.rb
+++ test/test.rb
@@ -7,23 +7,20 @@ rescue LoadError
 	abort "Make sure string_dot_levenshtein is installed on your system before testing"
 end
 
-words = IO.readlines(File.join(__dir__, 'words')).each(&:strip!)
-matches, i = {}, -1
+words = IO.readlines(File.join(__dir__, 'words')).each(&:strip!).shuffle.first(500)
+i = -1
 
 puts "Test started at #{Time.now}, please wait a moment... | Word length: #{words.length}"
 
 time = Process.clock_gettime(Process::CLOCK_MONOTONIC)
 while x = words[i += 1]
-	m = []
 	j = -1
 	while y = words[j += 1]
-		m << y if x.levenshtein(y) < 2
+		_l = x.levenshtein(y)
+		_l_rb = x.levenshtein_rb(y)
+		raise RuntimeError unless _l == _l_rb
 	end
-
-	matches.merge!(x => m)
 end
 time_diff = Process.clock_gettime(Process::CLOCK_MONOTONIC) - time
 
-puts matches, "\nWords: #{words.length} "\
-"| Matches: #{matches.values.map(&:length).sum} "\
-"| Time taken: #{time_diff}s"
+puts "Time taken: #{time_diff}s"
